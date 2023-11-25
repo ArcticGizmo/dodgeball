6c85c4dc0c81a655cf8f40f6d74e757c16cf6dfb
ArcticGizmo <jon.howell314@gmail.com>
2023-11-25 22:52:34 +0800

    able to detect ball collisions and "reflect" them

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -10,3 +10,19 @@ func _process(_delta):
 
 	# Point towards mouse
 	look_at(get_global_mouse_position())
+
+	if Input.is_action_just_pressed("secondary"):
+		handle_catchable_projectile()
+		$CatchArea.get_overlapping_areas()
+	
+func handle_catchable_projectile():
+	# TODO: might want to replace with enter/exit signals later on for performance	
+	var bodies = $CatchArea.get_overlapping_bodies()
+	# TODO: filter the bodies by class type
+	
+	if len(bodies) == 0:
+		return
+		
+	var projectile = bodies[0] as RigidBody2D;
+	projectile.linear_velocity *= -1
+	
--- scenes/projectiles/ball.gd
+++ scenes/projectiles/ball.gd
@@ -1,1 +1,9 @@
 extends RigidBody2D
+
+@export var max_speed: int = 500
+
+func _process(_delta):
+	
+	# limit max speed
+	if linear_velocity.length() > max_speed:
+		linear_velocity = linear_velocity.normalized() * max_speed
