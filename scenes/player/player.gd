25e46fd6cc02893aefaba8890edbcc09ce39df3e
ArcticGizmo <jon.howell314@gmail.com>
2023-11-26 19:37:57 +0800

    added item pickup zone

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,12 +2,12 @@ extends CharacterBody2D
 
 @export var max_speed: int = 500;
 @export var return_bonus_speed_factor: float = 1.5
+@export var throw_speed: int = 500
 var speed: int = max_speed;
 
-const ballTemplate: PackedScene = preload("res://scenes/projectiles/ball.tscn")
-var ballHeld: bool = false
-var ballParent: Node
-var ballCaughtSpeed: float
+const ball_template: PackedScene = preload("res://scenes/projectiles/ball.tscn")
+var ball_held: bool = false
+var ball_caught_speed: float
 
 func _ready():
 	$HeldItemSprite.visible = false
@@ -23,15 +23,16 @@ func _process(_delta):
 	# Handle catching the ball
 	if Input.is_action_just_pressed("secondary"):
 		handle_catchable_projectile()
+		if !ball_held:
+			maybe_pick_up_item()
 	
-	if Input.is_action_just_pressed("primary") and ballHeld:
-		throw_ball()
+	if Input.is_action_just_pressed("primary") and ball_held:
+			throw_ball()
 		
 	
 func handle_catchable_projectile():
 	# TODO: might want to replace with enter/exit signals later on for performance	
-	var bodies = $CatchArea.get_overlapping_bodies()
-	# TODO: filter the bodies by class type
+	var bodies = $CatchArea.get_overlapping_bodies().filter(func(x): return x.is_in_group("catchable"))
 	
 	if len(bodies) == 0:
 		return
@@ -40,10 +41,9 @@ func handle_catchable_projectile():
 		
 		
 	var projectile = bodies[0] as RigidBody2D;
-	ballCaughtSpeed = projectile.linear_velocity.length()
-	ballParent = projectile.get_parent()
+	ball_caught_speed = projectile.linear_velocity.length()
 	projectile.queue_free()
-	ballHeld = true
+	ball_held = true
 	$HeldItemSprite.visible = true
 	
 	# start bonus speed timer
@@ -53,17 +53,31 @@ func handle_catchable_projectile():
 func throw_ball():
 	$HeldItemSprite.visible = false	
 	
-	var ball = ballTemplate.instantiate()
-	ballParent.add_child(ball)
+	var ball = ball_template.instantiate()
+	%Projectiles.add_child(ball)
 	
 	ball.global_position = $ThrowFromMarker.global_position
 		
-	ball.linear_velocity = get_player_direction_as_vector() * ballCaughtSpeed
+	ball.linear_velocity = get_player_direction_as_vector()
 	if ($ReturnBonusSpeedTimer.time_left > 0):
-		ball.linear_velocity *= return_bonus_speed_factor
+		ball.linear_velocity *= ball_caught_speed * return_bonus_speed_factor
+	else:
+		ball.linear_velocity *= throw_speed
 	
-	ballHeld = false
-	ballParent = null
+	ball_held = false
+	$ReturnBonusSpeedTimer.stop()
+	
+func maybe_pick_up_item():
+	var triggers = $PickupArea.get_overlapping_areas().filter(func(x): return x.is_in_group("pickup-zones"))
+	
+	if len(triggers) == 0:
+		return
+		
+	print("pickup")
+	
+	ball_held = true
+	$HeldItemSprite.visible = true	
+	
 	
 
 func get_player_direction_as_vector():
