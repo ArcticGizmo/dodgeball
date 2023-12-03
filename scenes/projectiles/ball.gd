8a64cf5b8a9df96585443d73fc3dce0432f9bfc5
ArcticGizmo <jon.howell314@gmail.com>
2023-12-03 21:42:05 +0800

    improved friction

--- scenes/projectiles/ball.gd
+++ scenes/projectiles/ball.gd
@@ -4,15 +4,19 @@ class_name Ball
 
 @export var min_damage_speed = 500
 @export var frictionless: bool = false;
+@export var friction: int = 0;
 
 var throw_factor: int = 0
 
 func _ready():
 	self.linear_damp = 0
-	self.physics_material_override.friction = 0
-	self.physics_material_override.bounce = 1
+#	if (self.frictionless):
+#		self.physics_material_override.friction = 0
+#		self.physics_material_override.bounce = 1
 
 func _physics_process(_delta):
 	if (self.linear_velocity.length() < min_damage_speed):
 		throw_factor = 1
-
+	
+	self.apply_impulse(-self.linear_velocity.normalized() * friction * _delta)
+	
